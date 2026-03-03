ae43c599ee0cfb921736720bb4964942fc6ead74
cihatar <cihatarman2003@gmail.com>
2026-03-03 13:18:37 +0300

    refactor help functions and fix alignment

--- help.go
+++ help.go
@@ -5,55 +5,52 @@ import (
 	"strings"
 )
 
+type row struct {
+	left      string
+	right     string
+	leftWidth int
+}
+
+const tWidth = 80
+const maxKeyWidth = 25
+
 // Help generates and returns the global help menu for the application.
 // It includes usage instructions, a list of commands, global flags,
 // and is displayed when the user types --help or -h or when no command is provided.
 func (a *App) Help() string {
 	var sb strings.Builder
-	maxLen := getMaxLength(a.commands, a.globalFlags, true)
+
+	cmdRows := commandsToRows(a.commands)
+	flagRows := flagsToRows(a.globalFlags)
+
+	flagRows = append(flagRows,
+		row{left: "-h, --help", right: "Show help", leftWidth: 10},
+		row{left: "-v, --version", right: "Show version", leftWidth: 13},
+	)
 
 	// Usage
-	fmt.Fprintf(&sb, "Usage:\n  "+a.name)
+	sb.WriteString("Usage:\n  " + a.name)
 
 	if len(a.globalFlags) > 0 {
-		fmt.Fprintf(&sb, " [global flags]")
+		sb.WriteString(" [global flags]")
 	}
 	if len(a.commands) > 0 {
-		fmt.Fprintf(&sb, " [command]")
+		sb.WriteString(" [command]")
 	}
-	fmt.Fprintf(&sb, "\n")
+	sb.WriteString("\n")
 
 	// Description
-	if a.description != "" {
-		appDescPart := wrapText(a.description, 0)
-		fmt.Fprintf(&sb, "\n%s\n", appDescPart)
-	}
+	writeDescription(&sb, a.description)
 
 	// Commands
-	if len(a.commands) > 0 {
-		fmt.Fprintf(&sb, "\nCommands:\n")
-		sb.WriteString(writeCommands(a.commands, maxLen))
-	}
+	writeSection(&sb, "Commands", cmdRows)
 
 	// Global flags
-	fmt.Fprintf(&sb, "\nGlobal flags:\n")
-	if len(a.globalFlags) > 0 {
-		sb.WriteString(writeFlags(a.globalFlags, maxLen))
-		fmt.Fprintf(&sb, "\n")
-	}
-
-	// Help and version flags
-	hPart := fmt.Sprintf("  %s --%s", "-h,", "help")
-	fmt.Fprintf(&sb, "%-*s  %s\n", maxLen, hPart, "Show help")
-
-	if a.version != "" {
-		vPart := fmt.Sprintf("  %s --%s", "-v,", "version")
-		fmt.Fprintf(&sb, "%-*s  %s\n", maxLen, vPart, "Show version")
-	}
+	writeSection(&sb, "Global Flags", flagRows)
 
 	// Footer
 	if len(a.commands) > 0 {
-		fmt.Fprintf(&sb, "\nUse \"%s [command] --help\" for more information about a command.\n", a.name)
+		sb.WriteString("\nUse \"" + a.name + " [command] --help\" for more information about a command.\n")
 	}
 
 	return sb.String()
@@ -64,122 +61,43 @@ func (a *App) Help() string {
 // and is displayed when the user types "[command] --help" or "[command] -h".
 func (a *App) CommandHelp(cmd *Command) string {
 	var sb strings.Builder
-	maxLen := getMaxLength(cmd.subcommands, cmd.flags, false)
+
+	cmdRows := commandsToRows(cmd.subcommands)
+	flagRows := flagsToRows(cmd.flags)
 
 	// Usage
-	fmt.Fprintf(&sb, "Usage:\n  "+a.name)
-	fmt.Fprintf(&sb, "%s", cmd.fullPath())
+	sb.WriteString("Usage:\n  " + a.name)
+	sb.WriteString(cmd.fullPath())
 
 	if len(cmd.subcommands) > 0 {
-		fmt.Fprint(&sb, " [command]")
+		sb.WriteString(" [command]")
 	}
 	if len(cmd.flags) > 0 {
-		fmt.Fprint(&sb, " [flags]")
+		sb.WriteString(" [flags]")
 	}
 
 	if cmd.maxArg == 1 {
-		fmt.Fprint(&sb, " [arg]")
+		sb.WriteString(" [arg]")
 	} else if cmd.maxArg > 1 {
-		fmt.Fprintf(&sb, " [arg1...arg%d]", cmd.maxArg)
+		sb.WriteString(fmt.Sprintf(" [arg1...arg%d]", cmd.maxArg))
 	} else if cmd.minArg > 0 {
-		fmt.Fprint(&sb, " [args...]")
+		sb.WriteString(" [args...]")
 	}
 	fmt.Fprintf(&sb, "\n")
 
 	// Description
-	if cmd.long != "" {
-		longPart := wrapText(cmd.long, 0)
-		fmt.Fprintf(&sb, "\n%s\n", longPart)
-	}
+	writeDescription(&sb, cmd.long)
 
 	// Commands
-	if len(cmd.subcommands) > 0 {
-		fmt.Fprintf(&sb, "\nCommands:\n")
-		sb.WriteString(writeCommands(cmd.subcommands, maxLen))
-	}
+	writeSection(&sb, "Commands", cmdRows)
 
 	// Flags
-	if len(cmd.flags) > 0 {
-		fmt.Fprintf(&sb, "\nFlags:\n")
-		sb.WriteString(writeFlags(cmd.flags, maxLen))
-	}
+	writeSection(&sb, "Flags", flagRows)
 
 	return sb.String()
 }
 
-func writeCommands(cmds []*Command, keyWidth int) string {
-	var sb strings.Builder
-	for _, cmd := range cmds {
-		namePart := "  " + cmd.name
-		if cmd.alias != "" {
-			namePart += ", " + cmd.alias
-		}
-		descPart := wrapText(cmd.short, keyWidth+2)
-		fmt.Fprintf(&sb, "%-*s  %s\n", keyWidth, namePart, descPart)
-	}
-	return sb.String()
-}
-
-func writeFlags(flags []FlagInfo, keyWidth int) string {
-	var sb strings.Builder
-	for _, f := range flags {
-		aliasPart := "   "
-		if f.Alias() != "" {
-			aliasPart = "-" + f.Alias() + ","
-		}
-
-		defaultPart := ""
-		if f.DefaultValue().String() != zeroValues[f.HelpType()] {
-			defaultPart = fmt.Sprintf("(default: %s)", f.DefaultValue().String())
-		}
-
-		helpType := f.HelpType()
-		if helpType == "bool" {
-			helpType = ""
-		}
-
-		flagPart := fmt.Sprintf("  %s --%s %s", aliasPart, f.Name(), helpType)
-		descPart := wrapText(f.Description()+" "+defaultPart, keyWidth+2)
-
-		fmt.Fprintf(&sb, "%-*s  %s\n", keyWidth, flagPart, descPart)
-	}
-	return sb.String()
-}
-
-func getMaxLength(cmds []*Command, flags []FlagInfo, isRoot bool) int {
-	maxLen := 0
-	padding := 2
-	maxWidth := 25
-	aliasLen := 3 // -x,
-
-	if isRoot {
-		maxLen = 15
-	}
-
-	for _, cmd := range cmds {
-		l := len(cmd.name) + len(cmd.alias)
-		if l > maxLen {
-			maxLen = l
-		}
-	}
-
-	for _, f := range flags {
-		l := 2 + aliasLen + 1 + len(f.Name()) + 1 + len(f.HelpType())
-		if l > maxLen {
-			maxLen = l
-		}
-	}
-
-	if maxLen > maxWidth {
-		maxLen = maxWidth
-	}
-
-	return maxLen + padding
-}
-
-func wrapText(text string, keyWidth int) string {
-	tWidth := 80
-
+func wrap(text string, indent int, leftExceeds bool) string {
 	words := strings.Fields(strings.TrimSpace(text))
 	if len(words) == 0 {
 		return ""
@@ -187,12 +105,15 @@ func wrapText(text string, keyWidth int) string {
 
 	var result strings.Builder
 	var currLine strings.Builder
-	indent := strings.Repeat(" ", keyWidth)
+
+	if leftExceeds {
+		result.WriteString("\n" + strings.Repeat(" ", indent))
+	}
 
 	for _, word := range words {
 		// Start a new line if it exceeds width
-		if currLine.Len()+len(word)+1 > tWidth-keyWidth {
-			result.WriteString(currLine.String() + "\n" + indent)
+		if currLine.Len()+len(word)+1 > tWidth-indent {
+			result.WriteString(currLine.String() + "\n" + strings.Repeat(" ", indent))
 			currLine.Reset()
 		}
 
@@ -205,6 +126,88 @@ func wrapText(text string, keyWidth int) string {
 
 	result.WriteString(currLine.String())
 	return result.String()
+}
+
+func writeRow(left, right string, leftExceeds bool, maxKeyLen int) string {
+	return fmt.Sprintf("  %-*s  %s\n", maxKeyLen, left, wrap(right, maxKeyLen+4, leftExceeds))
+}
+
+func commandsToRows(cmds []*Command) []row {
+	rows := make([]row, 0)
+
+	for _, c := range cmds {
+		left := c.name
+		if c.alias != "" {
+			left += ", " + c.alias
+		}
+		rows = append(rows, row{left, c.short, len(left)})
+	}
+
+	return rows
+}
+
+func flagsToRows(flags []FlagInfo) []row {
+	rows := make([]row, 0)
+
+	for _, f := range flags {
+		left := ""
+		if alias := f.Alias(); alias != "" {
+			left = fmt.Sprintf("-%s, ", alias)
+		} else {
+			left = "    "
+		}
+
+		left += "--" + f.Name()
+
+		switch f.Value().Get().(type) {
+		case bool:
+		default:
+			if f.Metavar() != "" {
+				left += " " + f.Metavar()
+			}
+		}
+
+		right := f.Description()
+		if def := f.DefaultValue().String(); def != zeroValues[f.Metavar()] {
+			right += fmt.Sprintf(" (default: %s)", def)
+		}
+
+		rows = append(rows, row{left, right, len(left)})
+	}
+
+	return rows
+}
+
+func getMaxKeyLen(rows []row) int {
+	max := 0
+	for _, r := range rows {
+		if r.leftWidth > max {
+			max = r.leftWidth
+		}
+	}
+	if max > maxKeyWidth {
+		max = maxKeyWidth
+	}
+	return max
+}
+
+func writeSection(sb *strings.Builder, title string, rows []row) {
+	if len(rows) == 0 {
+		return
+	}
+	maxKeyLen := getMaxKeyLen(rows)
+	sb.WriteString("\n" + title + ":\n")
+	for _, r := range rows {
+		leftExceeds := r.leftWidth > maxKeyWidth
+		sb.WriteString(writeRow(r.left, r.right, leftExceeds, maxKeyLen))
+	}
+}
+
+func writeDescription(sb *strings.Builder, text string) {
+	if text == "" {
+		return
+	}
+	sb.WriteString("\n" + wrap(text, 0, false) + "\n")
 }
 
 func (c *Command) fullPath() string {
